61f65d855306693818fb86ddb8007d2982942bac
OnoprienkoArtem <o.artem.e@gmail.com>
2021-04-24 22:56:50 +0300

    add mongoos and models

---
+++ models/Position.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const positionSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  cost: {
+    type: Number,
+    required: true
+  },
+  category: {
+    ref: 'categories',
+    type: Schema.Types.ObjectId
+  },
+  user: {
+    ref: 'users',
+    type: Schema.Types.ObjectId
+  }
+});
+
+const positionsModel = mongoose.model('positions', positionSchema);
+
+export default positionsModel;
