e58bd7c15c0127c3342a878fbe8d1dbaef01075b
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-01 22:59:15 +0300

    work with position routes

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,39 @@
-export function getByCategoryId(req, res) {
-  res.status(200).json()
+import Position from '../models/position.js';
+import { errorHandler } from '../utils/errorHandler.js';
+
+export async function getByCategoryId(req, res) {
+  try {
+    const positions = await Position.find({
+      category: req.params.categoryId,
+      user: req.user.id,
+    });
+
+    res.status(200).json(positions);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
 export function remove(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
 export function create(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
 export function update(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
