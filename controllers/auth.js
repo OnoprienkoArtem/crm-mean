320a4fc2cf1b743d89b834075d264456164e4a31
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-01 22:19:54 +0300

    add error handler

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import keys from '../config/keys.js';
+import { errorHandler } from '../utils/errorHandler.js';
 
 export async function login(req, res) {
   const candidate = await User.findOne({
@@ -52,7 +53,7 @@ export async function register(req, res) {
       await user.save();
       res.status(201).json(user);
     } catch (e) {
-
+      errorHandler(res, e);
     }
   }
 }
