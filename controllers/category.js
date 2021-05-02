eec6f3e14775ddf59cf2d257d7aa9e808c542ba9
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-02 23:33:58 +0300

    work with categories routes, add logic for getall

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,46 @@
-export function getAll(req, res) {
-  res.status(200).json({message: 'Categories'});
+import Category from '../models/category.js';
+import { errorHandler } from '../utils/errorHandler.js';
+
+export async function getAll(req, res) {
+  try {
+    const categories = await Category.find({
+      user: req.user.id,
+    });
+
+    res.status(200).json(categories);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
 export function getById(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
 export function remove(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
 export function create(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
 export function update(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
