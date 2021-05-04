cd105a0965beade06e3e3b8d1ca00c36b301eab1
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-04 23:01:56 +0300

    set middleware to routes for create and update

--- roures/category.js
+++ roures/category.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import {getAll, getById, remove, create, update} from '../controllers/category.js';
 import passport from 'passport';
+
+import upload from '../middleware/upload.js';
+import {getAll, getById, remove, create, update} from '../controllers/category.js';
+
 
 const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', {session: false}), getAll);
 router.get('/:id', getById);
 router.delete('/:id', remove);
-router.post('/', create);
-router.patch('/:id', update);
+router.post('/', upload.single('image'), create);
+router.patch('/:id', upload.single('image'), update);
 
 
 export default router;
