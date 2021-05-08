98d107c2abe768671e63dab167f2a972d5ba651e
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-08 22:49:58 +0300

    add logic for create method

--- controllers/order.js
+++ controllers/order.js
@@ -1,7 +1,30 @@
+import Order from '../models/Order.js';
+import {errorHandler} from '../utils/errorHandler.js';
+
 export function getAll(req, res) {
-  res.status(200).json()
+  try {
+
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
 
-export function create(req, res) {
-  res.status(200).json()
+export async function create(req, res) {
+  try {
+    const lastOrder = await Order
+      .findOne({user: req.user.id})
+      .sort({date: -1});
+
+    const maxOrder = lastOrder ? lastOrder.order : 0;
+
+    const order = await Order({
+      list: req.body.list,
+      user: req.user.id,
+      order: maxOrder + 1
+    }).save();
+
+    res.status(201).json(order);
+  } catch (e) {
+    errorHandler(res, e);
+  }
 }
