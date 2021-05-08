e0b38b75297111831e5197505195e63a49501c03
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-08 22:08:50 +0300

    add for position and order jwt strategy

--- roures/order.js
+++ roures/order.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import passport from 'passport';
+
 import {getAll, create} from '../controllers/order.js';
+
 
 const router = express.Router();
 
-router.get('/', getAll);
-router.post('/', create);
-
+router.get('/', passport.authenticate('jwt', {session: false}), getAll);
+router.post('/', passport.authenticate('jwt', {session: false}), create);
 
 export default router;
--- roures/position.js
+++ roures/position.js
@@ -1,12 +1,14 @@
 import express from 'express';
+import passport from 'passport';
+
 import {getByCategoryId, remove, create, update} from '../controllers/position.js';
+
 
 const router = express.Router();
 
-router.get('/:categoryId', getByCategoryId);
-router.post('/', create);
-router.delete('/:id', remove);
-router.patch('/:id', update);
-
+router.get('/:categoryId', passport.authenticate('jwt', {session: false}), getByCategoryId);
+router.post('/', passport.authenticate('jwt', {session: false}), create);
+router.delete('/:id', passport.authenticate('jwt', {session: false}), remove);
+router.patch('/:id', passport.authenticate('jwt', {session: false}), update);
 
 export default router;
