c169f8f7233d08227153fb334a5744fed8a8504e
OnoprienkoArtem <o.artem.e@gmail.com>
2021-05-18 22:46:15 +0300

    add auth service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,8 +9,8 @@ import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.comp
 import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component';
 import {RegisterPageComponent} from './register-page/register-page.component';
 import {CheckFormFieldValidityDirective} from './shared/directives/check-form-field-validity.directive';
-import { FormErrorComponent } from './shared/form-error/form-error.component';
-import { FormErrorPipe } from './shared/pipes/form-error.pipe';
+import {FormErrorComponent} from './shared/form-error/form-error.component';
+import {FormErrorPipe} from './shared/pipes/form-error.pipe';
 
 @NgModule({
   declarations: [
