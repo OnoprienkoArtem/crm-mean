32f62e5b2e555be93fc4b684d88c1a309b923913
OnoprienkoArtem <o.artem.e@gmail.com>
2021-06-09 23:20:50 +0300

    done logout button

--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '@app/shared/services/auth.service';
 
 interface SideMenu {
   url: string;
@@ -20,10 +23,16 @@ export class SiteLayoutComponent implements OnInit {
     {url: '/categories', name: 'Categories'},
   ];
 
-  constructor() {
+  constructor(private auth: AuthService, private router: Router) {
   }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
   }
 
+  public logout(e: Event): void {
+    e.preventDefault();
+
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
