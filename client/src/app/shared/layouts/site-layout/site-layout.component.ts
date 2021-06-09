d6d08f66bcbe8f979953df9a0eac416603b258cf
OnoprienkoArtem <o.artem.e@gmail.com>
2021-06-09 23:10:51 +0300

    done side navigaition

--- client/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+interface SideMenu {
+  url: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-site-layout',
   templateUrl: './site-layout.component.html',
-  styleUrls: ['./site-layout.component.scss']
+  styleUrls: [ './site-layout.component.scss' ]
 })
 export class SiteLayoutComponent implements OnInit {
 
-  constructor() { }
+  public links: Array<SideMenu> = [
+    {url: '/overview', name: 'Overview'},
+    {url: '/analytics', name: 'Analytics'},
+    {url: '/history', name: 'History'},
+    {url: '/order', name: 'Add order'},
+    {url: '/categories', name: 'Categories'},
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
