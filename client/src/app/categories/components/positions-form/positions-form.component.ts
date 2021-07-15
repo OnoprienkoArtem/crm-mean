b1b1ae1f589ef134d135ac47818a80c547df3256
OnoprienkoArtem <o.artem.e@gmail.com>
2021-07-15 23:42:59 +0300

    add materialize modal for creating position

--- client/src/app/categories/components/positions-form/positions-form.component.ts
+++ client/src/app/categories/components/positions-form/positions-form.component.ts
@@ -1,18 +1,21 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { PositionsService } from '@app/core/services';
 import { Position } from '@app/shared/interfaces';
+import { MaterializeModalInstance, MaterializeService } from '@app/shared/materialize/materialize.service';
 
 @Component({
   selector: 'app-positions-form',
   templateUrl: './positions-form.component.html',
   styleUrls: ['./positions-form.component.scss']
 })
-export class PositionsFormComponent implements OnInit {
+export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy {
   public positions: Position[] = [];
   public loading: boolean = false;
+  public modal: MaterializeModalInstance;
 
   @Input() categoryId: string
 
+  @ViewChild('modal') modalRef: ElementRef;
 
   constructor(private positionService: PositionsService) { }
 
@@ -24,4 +27,23 @@ export class PositionsFormComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  ngAfterViewInit(): void {
+    this.modal = MaterializeService.initModal(this.modalRef);
+  }
+
+  public onSelectPosition(position: Position): void {
+    this.modal.open();
+  }
+
+  public onAddPosition(): void {
+    this.modal.open();
+  }
+
+  public onCancel(): void {
+    this.modal.close();
+  }
 }
--- client/src/app/shared/materialize/materialize.service.ts
+++ client/src/app/shared/materialize/materialize.service.ts
@@ -1,17 +1,27 @@
 import { ElementRef } from '@angular/core';
 
-declare var M: any;
+declare let M: any;
+
+export interface MaterializeModalInstance {
+  open(): void;
+  close(): void;
+  destroy(): void;
+}
 
 export class MaterializeService {
-  static toast(message: string) {
+  static toast(message: string): void {
     M.toast({ html: message });
   }
 
-  static initializeFloatingButton(ref: ElementRef) {
+  static initializeFloatingButton(ref: ElementRef): void {
     M.FloatingActionButton.init(ref.nativeElement);
   }
 
-  static updateTextInputs() {
+  static updateTextInputs(): void {
     M.updateTextFields();
+  }
+
+  static initModal(ref: ElementRef): MaterializeModalInstance {
+    return M.Modal.init(ref.nativeElement);
   }
 }
