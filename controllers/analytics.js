b806265b8a52cc708e92a21c0c77593bcabb86f4
OnoprienkoArtem <o.artem.e@gmail.com>
2021-08-13 23:21:17 +0300

    add calculate price to analytic controller

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -50,5 +50,10 @@ function getOrdersMap(orders = []) {
 }
 
 function calculatePrice(orders = []) {
-
+  return orders.reduce((total, order) => {
+    const orderPrice = order.list.reduce((orderTotal, item) => {
+      return orderTotal = item.cost * item.quantity;
+    }, 0);
+    return total += orderPrice;
+  }, 0);
 }
